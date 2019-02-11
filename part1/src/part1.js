3503acb43ca66e44b2f2737aa217b62f7af3143b
TJ <tjchase34@gmail.com>
2019-02-11 15:45:24 -0500

    Added login page, database functionality, create user page, login stats on index.html

--- part1/src/part1.js
+++ part1/src/part1.js
@@ -14,6 +14,8 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/sellMyStuff", { useNewUrlParser: true });
 
+var currLoggedIn = '';
+
 var stuffSchema = new mongoose.Schema({
     user_id: Number,
     item_id: Number,
@@ -22,7 +24,14 @@ var stuffSchema = new mongoose.Schema({
     item_status: String
 });
 
+var loginSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    balance: Number
+});
+
 var stuff = mongoose.model("stuff", stuffSchema);
+var login = mongoose.model("login", loginSchema);
 
 app.post("/addStuff", (req, res) => {
     var data = new stuff(req.body);
@@ -43,6 +52,53 @@ app.post("/getStuff", (req, res) => {
             console.log(stuff);
             res.send(stuff);
         }
+    });
+});
+
+app.post("/getUser", (req, res) => {
+    login.findOne({username: currLoggedIn}, function (err, user) {
+        if (user) {
+                console.log("User found!");
+                console.log(user);
+                res.send(user);
+        }
+        else {
+            console.log("Woah something went horribly wrong");
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    login.findOne({username: req.body.username}, function (err, user) {
+        if (user) {
+            if (req.body.password == user.password) {
+                console.log("Login successful!");
+                currLoggedIn = req.body.username;
+                res.send({valid: true});
+            }
+            else {
+                console.log("Incorrect Password!");
+                res.send({valid: false});
+            }
+        }
+        else {
+            console.log("User not found!");
+            res.send({valid: false});
+        }
+    });
+});
+
+app.post("/createUser", (req, res) => {
+    console.log(req.body);
+    var data = new login(req.body);
+    data.save()
+    .then(item => {
+        res.send("User created and saved to database");
+        console.log("Saved user to database");
+        console.log(data);
+    })
+    .catch(err => {
+        res.status(400).send("unable to save to database");
     });
 });
 
