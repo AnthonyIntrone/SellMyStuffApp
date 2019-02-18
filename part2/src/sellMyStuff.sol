f3aa92c0d35f7f5dab12eb482c57e5893c742da8
TJ <tjchase34@gmail.com>
2019-02-18 17:59:28 -0500

    Started smart contract.

---
+++ part2/src/sellMyStuff.sol
@@ -0,0 +1,34 @@
+// Part 2: Next add a smart contract and the decentralized elements to that will record all the transactions
+// including user management on the blockchain. The transactions could be: register, unregister, buy,
+// settlePayment, adddeposit etc. Develop and test this smart contract independently. Difficulty: Medium.
+
+pragma solidity ^0.5.0;
+
+contract sellMyStuff {
+    
+    /*** State variables ***/
+    
+    address public user;
+    mapping (address => uint) private balances;
+    
+    /*** Constructor ***/
+
+    // 'Registers' new user
+    constructor() public {
+        user = msg.sender;
+        balances[user] = 0;
+    }
+    
+    /*** Functions ***/
+    
+    function deposit(uint amount) public {
+        uint current_amount = balances[user];
+        balances[user] = current_amount + amount;
+    }
+    
+    
+
+
+
+
+}
