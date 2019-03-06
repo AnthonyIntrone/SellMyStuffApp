6b13f4f57f4daa1c26619e4528893f195c78c079
TJ <tjchase34@gmail.com>
2019-03-06 17:26:11 -0500

    Updated smart contract and README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# 410-project1
+# Sell My Stuff - Part 1/2
+
+A completely decentralized marketplace running on the Ethereum blockchain.
+
+## How to run the web application (part 1)
+
+### Linux Instructions
+1. Make sure MongoDB is installed for your system, and the `mongod` daemon is running.
+2. `cd` into the `part1/src/` directory.
+3. Install all node dependencies by executing `npm install`.
+4. Execute `npm start` to start the web application on `localhost:8080` (modifiable by editing `part1.js`).
+
+### Windows Instructions
+1. Install Linux.
+2. Follow Linux instructions above.
+
+## Viewing the Solidity smart contract (part 2) 
+
+Our smart contract can be viewed under `part2/src/sellMyStuff.sol`
+
+## Authors
+
+* **TJ Chase**
+* **Anthony Introne**
--- part2/src/sellMyStuff.sol
+++ part2/src/sellMyStuff.sol
@@ -1,38 +1,55 @@
-// Part 2: Next add a smart contract and the decentralized elements to that will record all the transactions
-// including user management on the blockchain. The transactions could be: register, unregister, buy,
-// settlePayment, adddeposit etc. Develop and test this smart contract independently. Difficulty: Medium.
-
 pragma solidity ^0.5.0;
 
 contract sellMyStuff {
     
     /*** State variables ***/
     
-    address public user;
-    mapping (address => uint) private balances;
+    address private admin;
+    mapping (address => int) private balances;
+    
+    modifier onlyAdmin {
+        require(
+            msg.sender == admin,
+            "Only the admin can call this function."
+        );
+        _;
+    }
     
     /*** Constructor ***/
 
     // 'Registers' new user
     constructor() public {
-        user = msg.sender;
-        balances[user] = 0;
+        admin = msg.sender;
+        balances[admin] = 0;
     }
     
     /*** Functions ***/
     
-    function deposit(uint amount) public {
-        uint current_amount = balances[user];
+    function addUser(address user) private onlyAdmin {
+        balances[user] = 0;
+    }
+    
+    function removeUser(address user) private onlyAdmin {
+        delete balances[user];
+    }
+    
+    function deposit(int amount, address user) public {
+        int current_amount = balances[user];
         balances[user] = current_amount + amount;
     }
     
-    function getAccountBalance() public view returns (uint) {
+    function withdraw(int amount, address user) public {
+        int current_amount = balances[user];
+        balances[user] = current_amount + amount;
+    }
+    
+    function checkBalance(address user) public view returns (int) {
         return balances[user];
     }    
     
-    function transaction(uint item, uint cost, address recipient, address seller) public {
+    function transaction(int cost, address recipient, address seller) public {
         assert(balances[recipient] > cost);
-        balances[recipient] =  balances[recipient] - cost;
+        balances[recipient] = balances[recipient] - cost;
         balances[seller] = balances[seller] + cost;
     }
 }
